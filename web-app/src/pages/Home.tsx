5050f2a24eeee2416ffc118838e47ea3cd6473d8
P-Karsten <pascal.karsten@smail.th-koeln.de>
2024-06-22 18:48:52 +0200

    auth/aut fix

--- web-app/src/pages/Home.tsx
+++ web-app/src/pages/Home.tsx
@@ -1,32 +1,65 @@
 // Home.tsx
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import VideoLoader from "../components/video/VideoLoader";
 import './home.css';
-import ReactDOM from 'react-dom';
-import App1 from '../App';
 import keycloak from './keycloak';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 
-
-
-
-const Home = ({ userInfo }) => {
+const Home = () => {
+  const [token, setToken] = useState<string | null>(null);
   const [videoPaths, setVideoPaths] = useState<string[]>([]);
+  const [author, setAuthor] = useState(false);
 
   useEffect(() => {
-    const videoFiles: string[] = ['VideoA.mp4', 'VideoB.mp4', 'VideoC.mp4']; // VideoFile names
+    const storedToken = localStorage.getItem('accessToken');
+    setToken(storedToken);
 
-    const repeatedVideoFiles = videoFiles.flatMap(file => Array.from({ length: 1 }, () => file));
+    const checkAccess = async (token: string | null) => {
+      if (!token) return;
 
-    for (let i = repeatedVideoFiles.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [repeatedVideoFiles[i], repeatedVideoFiles[j]] = [repeatedVideoFiles[j], repeatedVideoFiles[i]];
-    }
+      const resource = 'Videos1'; // Specify the resource you want to check
 
-    setVideoPaths(repeatedVideoFiles.map(file => `/videos/${file}`));
+      try {
+        const response = await fetch('http://localhost:8080/realms/master/protocol/openid-connect/token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${token}`
+          },
+          body: new URLSearchParams({
+            'grant_type': 'urn:ietf:params:oauth:grant-type:uma-ticket',
+            'audience': 'real-client',
+            'permission': resource
+          })
+        });
+
+        if (response.ok) {
+          setAuthor(true);
+          const data = await response.json();
+          console.log('Access granted:', data);
+          loadVideos();
+        } else {
+          console.log('Access denied');
+        }
+      } catch (error) {
+        console.error('Error checking access:', error);
+      }
+    };
+
+    const loadVideos = () => {
+      const videoFiles: string[] = ['VideoA.mp4', 'VideoB.mp4', 'VideoC.mp4']; // VideoFile names
+
+      const repeatedVideoFiles = videoFiles.flatMap(file => Array.from({ length: 1 }, () => file));
+
+      for (let i = repeatedVideoFiles.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [repeatedVideoFiles[i], repeatedVideoFiles[j]] = [repeatedVideoFiles[j], repeatedVideoFiles[i]];
+      }
+
+      setVideoPaths(repeatedVideoFiles.map(file => `/videos/${file}`));
+    };
+
+    checkAccess(storedToken);
   }, []);
-
 
   return (
     <div className="container-main">
@@ -66,9 +99,5 @@ const Home = ({ userInfo }) => {
     </div>
   );
 };
-
-
-
-
 
 export default Home;
--- web-app/src/pages/Login.tsx
+++ web-app/src/pages/Login.tsx
@@ -4,28 +4,21 @@ import './login.css';
 
 
 
-import ReactDOM from 'react-dom';
-import App1 from '../App';
-import keycloak from './keycloak';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
-
-
 const Login = ({ setIsAuthenticated, setUserInfo }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const [acc_token, setAccToken] = useState()
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     const data = new URLSearchParams();
     data.append('client_id', 'real-client');
-    data.append('client_secret', 'zlaMF4ekGVzh75kccZl9nO2JQmuhjFrX');
+    data.append('client_secret', '4h8j9GtggcdUyGYtQcFVlFNS7pEKMLaY');
     data.append('username', username);
     data.append('password', password);
     data.append('grant_type', 'password');
-    
-  
     
     try {
       const response = await fetch("http://localhost:8080/realms/master/protocol/openid-connect/token", {
@@ -36,106 +29,20 @@ const Login = ({ setIsAuthenticated, setUserInfo }) => {
         },
         body: data.toString()
       });
-      console.log(data.toString())
 
       if (response.ok) {
         const result = await response.json();
         console.log('Authenticated', result); 
         localStorage.setItem('accessToken', result.access_token);
+        setAccToken(result.access_token)
+        navigate('/home');
 
-        fetchUserId(result.access_token);
       } else {
         const errorData = await response.json();
         console.log('Failed to authenticate', errorData);
       }
     } catch (error) {
       console.error('Login error', error);
-    }
-  };
-  
-  
-
-
-
-  
-  
-  
-  
-
-  const fetchUserId = async (token) => {
-    try {
-      const response = await fetch(`http://localhost:8080/admin/realms/master/users?username=${username}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (response.ok) {
-        const users = await response.json();
-        if (users.length > 0) {
-          const user = users[0];
-          fetchUserInfo(user.id, token);
-        } else {
-          console.log('User not found');
-        }
-      } else {
-        console.log('Failed to fetch user ID');
-      }
-    } catch (error) {
-      console.error('Fetch user ID error', error);
-    }
-  };
-
-  const fetchUserInfo = async (userId, token) => {
-    try {
-      const userResponse = await fetch(`http://localhost:8080/admin/realms/master/users/${userId}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      });
-
-      const groupsResponse = await fetch(`http://localhost:8080/admin/realms/master/users/${userId}/groups`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      });
-
-      const rolesResponse = await fetch(`http://localhost:8080/admin/realms/master/users/${userId}/role-mappings/realm`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (userResponse.ok && groupsResponse.ok && rolesResponse.ok) {
-        const user = await userResponse.json();
-        const groups = await groupsResponse.json();
-        const roles = await rolesResponse.json();
-
-        const userInfo = {
-          username: user.username,
-          id: user.id,
-          roles: roles.map(role => role.name),
-          groups: groups.map(group => group.name)
-        };
-
-        localStorage.setItem('userInfo', JSON.stringify(userInfo));
-        setUserInfo(userInfo);
-        setIsAuthenticated(true);
-
-        navigate('/home');
-      } else {
-        console.log('Failed to fetch user information');
-      }
-    } catch (error) {
-      console.error('Fetch user info error', error);
     }
   };
 
@@ -152,8 +59,5 @@ const Login = ({ setIsAuthenticated, setUserInfo }) => {
     </div>
   );
 };
-
-
-
 
 export default Login;
